8f0e0e3112ac8cd77633e1490874ec118420e145
Abner1309 <soul_abner@hotmail.com>
2026-07-15 17:37:27 -0300

    feat: Initial Script for the Classroom Creation  Screen

---
+++ Assets/Scripts/CreateClassroom.cs
@@ -0,0 +1,11 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class CreateClassroom : MonoBehaviour
+{
+    public void BackButton()
+    {
+        SceneManager.LoadScene("2-Profile_Screen");
+    }
+}
+
